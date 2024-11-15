91917ad67d3761e80ab64c13a618c70a1abe1889
Tyler Hays <tyhays@gmail.com>
2024-11-14 19:58:53 -0800

    Fix go build

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -1,6 +1,6 @@
 vim.api.nvim_set_keymap(
 	"n",
 	"<leader>;b",
-	"<cmd>lua ToggleFloatTerm('go run ' .. vim.fn.expand('%'))<CR>",
+	"<cmd>lua ToggleFloatTerm('go run ' .. vim.fn.expand('%'), nil, false)<CR>",
 	{ noremap = true, silent = true }
 )
