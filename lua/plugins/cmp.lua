19ba533168c9d2c4c6dbd7631d5d897d4d0cd78e
Tyler Hays <tyhays@gmail.com>
2024-11-16 19:44:08 -0800

    Add snippets

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -14,7 +14,7 @@ return {
     "chrisgrieser/nvim-scissors",
     dependencies = "nvim-telescope/telescope.nvim",
     opts = {
-      snippetDir = "./snippets",
+      snippetDir = vim.fn.stdpath("config") .. "/snippets",
     }
   },
 }
