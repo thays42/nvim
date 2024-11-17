f5df2833a61286b4feff5b73ae9f0d5996cae7ff
Tyler Hays <tyhays@gmail.com>
2024-11-16 19:44:19 -0800

    Add go test keymap

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -1,6 +1,13 @@
 vim.api.nvim_set_keymap(
-	"n",
-	"<leader>;b",
-	"<cmd>lua ToggleFloatTerm('go run ' .. vim.fn.expand('%'), nil, false)<CR>",
-	{ noremap = true, silent = true }
+  "n",
+  "<leader>;b",
+  "<cmd>lua ToggleFloatTerm('go run ' .. vim.fn.expand('%'), nil, false)<CR>",
+  { noremap = true, silent = true }
+)
+
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>;t",
+  "<cmd>lua ToggleFloatTerm('go test', vim.fn.expand('%:p:h'), false)<CR>",
+  { noremap = true, silent = true }
 )
