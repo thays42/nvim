bba3a1b3f6d4ad615aa98928a893ab7bf6c9fd3d
Tyler Hays <tyhays@gmail.com>
2024-11-16 19:44:28 -0800

    Add autoformatting

--- lua/after/lsp.lua
+++ lua/after/lsp.lua
@@ -30,10 +30,39 @@ vim.api.nvim_create_autocmd('LspAttach', {
   end,
 })
 
+local allow_format = function(servers)
+  return function(client) return vim.tbl_contains(servers, client.name) end
+end
+
+local buffer_autoformat = function(bufnr)
+  local group = 'lsp_autoformat'
+  vim.api.nvim_create_augroup(group, { clear = false })
+  vim.api.nvim_clear_autocmds({ group = group, buffer = bufnr })
+
+  vim.api.nvim_create_autocmd('BufWritePre', {
+    buffer = bufnr,
+    group = group,
+    desc = 'LSP format on save',
+    callback = function()
+      -- note: do not enable async formatting
+      vim.lsp.buf.format({ async = false, timeout_ms = 10000, filter = allow_format({ 'lua_ls', 'gopls' }) })
+    end,
+  })
+end
+
 vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(event)
-    local opts = { buffer = event.buf }
+    local id = vim.tbl_get(event, 'data', 'client_id')
+    local client = id and vim.lsp.get_client_by_id(id)
+    if client == nil then
+      return
+    end
 
+    if client.supports_method('textDocument/formatting') then
+      buffer_autoformat(event.buf)
+    end
+
+    local opts = { buffer = event.buf }
     vim.keymap.set({ 'n', 'x' }, 'gq', function()
       vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
     end, opts)
